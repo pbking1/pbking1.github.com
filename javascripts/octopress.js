05dcffd28757c45dd082b75b9e1987025f89e83e
pbking1 <731849410@qq.com>
2014-05-17 10:15:51 +0800

    Site updated at 2014-05-17 02:15:51 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -93,11 +93,9 @@ function flashVideoFallback(){
 
 function wrapFlashVideos() {
   $('object').each(function(i, object) {
-	if (object.attr('id') != "JIATHISSWF") {
     if( $(object).find('param[name=movie]').length ){
       $(object).wrap('<div class="flash-video">')
     }
-	}
   });
   $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video">')
 }
